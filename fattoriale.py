b9f4f154040bcd68d75305b2985e97d293338466
unknown <massimoorfeo.ermacora.24@stud.itsaltoadriatico.it>
2025-01-13 14:22:01 +0100

    fattoriale iterativo

--- fattoriale.py
+++ fattoriale.py
@@ -1,9 +1,8 @@
-num = 100
+num = 5
 
 def fattoriale(num, sum) :
-    if num != 1 :
-        sum *= num
-        sum = fattoriale(num - 1, sum)
+    if num > 1 :
+        sum = fattoriale(num - 1, sum * num)
     return sum
         
 print(fattoriale(num, 1))
